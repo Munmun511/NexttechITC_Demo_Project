148b54a16f493ee5e9e354e111e4a21f54161fed
abcd <test@gmail.com>
2023-09-27 07:43:52 -0500

    git practice

---
+++ src/test/java/demo2/datetype.java
@@ -0,0 +1,76 @@
+package demo2;
+
+public class datetype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number1=2;
+		
+		//declare decimal value
+		double number2=2.22;
+		
+		//store char
+		char alp='a';
+		char alp2='1';
+		
+	//store my name
+		String myname="Nayeem";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("My name is "+myname);
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		String word = "Munmun";
+		
+		System.out.println(word);
+		
+
+
+	}
+
+	}
+
