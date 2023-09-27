edbe3a63444e1b349c9eab33ed948781b50711e9
abcd <test@gmail.com>
2023-09-27 07:32:58 -0500

    git practice

---
+++ src/test/java/demo1/ifelse.java
@@ -0,0 +1,57 @@
+package demo1;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (99>69){
+			System.out.println("my grade is A");
+				}
+			int x=70;
+			int y=65;
+
+			if (x>y) {
+				System.out.println("My name is Munmun");
+			}
+
+			int a=90;
+			int b=100;
+
+			if (a>b) {
+				System.out.println("x is greater than y");
+			}
+
+			int time =30;
+
+			if (time<15) {
+				System.out.println("Good morning");
+
+			}
+			else if (time <25) {
+				System.out.println("good afternoon");
+			}
+			else {
+				System.out.println("good evening");
+			}
+
+
+			//Destination of states california or texas
+			int speedlimit =100;
+			String d= "California's neighborhood speedlimit";
+			String e = "Texas's neighborhood speedlimit";
+			String f= "Oklahoma's neighborhood speedlimit";
+
+			if (speedlimit <45) {
+				System.out.println(d);
+
+			}
+			else if (speedlimit <30) {
+				System.out.println(e);
+			}
+			else {
+				System.out.println(f);
+			}
+			
+	}
+
+}
